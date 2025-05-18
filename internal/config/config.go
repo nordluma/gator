09b30d215d66b4b096d5cf2f2c35e51f14c6fbfc
nordluma <maxnordlund.95@hotmail.com>
2025-05-18 22:17:34 +0300

    feat(config): read config and set current user

---
+++ internal/config/config.go
@@ -0,0 +1,67 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DbUrl           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func Read() Config {
+	configFile, err := getConfigFilePath()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		log.Fatalf("Could read %s. Error: %s", configFile, err)
+	}
+
+	var config Config
+	if err = json.Unmarshal(data, &config); err != nil {
+		log.Fatal("Could not deserialize '.gatorconfig.json'")
+	}
+
+	return config
+}
+
+func (c *Config) SetUser(user string) {
+	c.CurrentUserName = user
+	if err := write(*c); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("Could not get home dir: %s\n", err)
+	}
+
+	return fmt.Sprintf("%s/%s", homeDir, configFileName), nil
+}
+
+func write(cfg Config) error {
+	configFile, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+	data, err := json.Marshal(cfg)
+	if err != nil {
+		return fmt.Errorf("Could not serialize config. Error: %s", err)
+	}
+
+	if err = os.WriteFile(configFile, data, os.FileMode(os.O_TRUNC|os.O_CREATE)); err != nil {
+		return fmt.Errorf("Could not write config to file. Error: %s", err)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/nordluma/gator/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	cfg := config.Read()
+	cfg.SetUser("lane")
+	cfg = config.Read()
+
+	fmt.Println(cfg)
 }
