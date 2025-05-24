32ba8d13caec64bb4118d81be183ee3a0c0ed184
nordluma <maxnordlund.95@hotmail.com>
2025-05-24 16:25:29 +0300

    build(sql/queries): add query to delete a feed follow

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -18,3 +18,6 @@ SELECT feeds.name
 FROM feed_follows
 JOIN feeds ON feeds.id = feed_follows.feed_id
 WHERE feed_follows.user_id = $1;
+
+-- name: DeleteFeedFollowForUser :exec
+DELETE FROM feed_follows WHERE user_id = $1 AND feed_id = $2;
